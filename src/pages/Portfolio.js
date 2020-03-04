6840b6b6de6e4929ba7b9a283c08be06cd3c078c
Your Name <alexsigala87@gmail.com>
2020-03-03 19:25:55 -0800

    added jumbotron and carosel will work on adding links to each with images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Carousel from "../components/Carousel";
 class Portfolio extends Component {
     render() {
-        return <div>hello</div>;
+        return (
+            <div>
+                <Jumbotron>
+                    <h1>Get to Know your Next Engineer</h1>
+                    <p>
+                        I'm a people person, a JavaScript enthusiast, and an
+                        avid learner. I have a passion for code and for helping
+                        others. Just below you can visit some of my other pasion
+                        projects on teaching web-development and solutions to
+                        some common interview algos. Continue reading for a
+                        short bio on my journey as a dev and some personal
+                        projects I am proud of.
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </Jumbotron>
+                <Carousel></Carousel>
+            </div>
+        );
     }
 }
 export default Portfolio;
