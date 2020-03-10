6b17f6d2f522565552379b27fa3c570c7ed8942e
Your Name <alexsigala87@gmail.com>
2020-03-09 23:23:37 -0700

    added index to images where i export all the images ot make the importing more clean in other pages

---
+++ src/assets/images/index.js
@@ -0,0 +1,36 @@
+const algos = require("./alex'salgos.JPG");
+const background = require("./background.jpg");
+const bootstrap = require("./bootstrap.jpeg");
+const codingvid = require("./coding videos.JPG");
+const dunk = require("./dunk.jpg");
+const football = require("./football.jpg");
+
+const gify = require("./gify.png");
+const goku = require("./goku.jpg");
+const drawing = require("./josh drawing.jpg");
+const letters = require("./letters.jpg");
+const liri = require("./liri_prompts.JPG");
+const logos = require("./logos.jpg");
+const newsly = require("./newsly.JPG");
+const psychic = require("./psychic.jpg");
+const statsdash = require("./statsdash.JPG");
+const sqlstore = require("./SQL_store.JPG");
+
+module.exports = {
+    algos: algos,
+    background: background,
+    bootstrap: bootstrap,
+    codingvid: codingvid,
+    dunk: dunk,
+    football: football,
+    gify: gify,
+    goku: goku,
+    drawing: drawing,
+    letters: letters,
+    liri: liri,
+    logos: logos,
+    newsly: newsly,
+    psychic: psychic,
+    statsdash: statsdash,
+    sqlstore: sqlstore
+};
