ab0986049f9081f4d55ef99c2128eb587e61a943
Your Name <alexsigala87@gmail.com>
2020-04-29 00:00:33 -0700

    added to carosel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import tutorial from "../../assets/images/alex'salgos.JPG";
 function myCarousel() {
     return (
         <Carousel>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=373940"
+                    src={tutorial}
+                    target="_blank"
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
+                    <h3>My Tutorials</h3>
                     <p>
-                        Nulla vitae elit libero, a pharetra augue mollis
-                        interdum.
+                        Learn Full-Stack Web development using javascript,
+                        jQuery and React
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
